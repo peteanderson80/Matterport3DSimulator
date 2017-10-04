0530e626817fd169e42b3c4864e6477e7139283f
Phil Roberts <philip.roberts@adelaide.edu.au>
2017-10-04 16:46:41 +1030

    Support location change in python driver

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -12,15 +12,23 @@ sim.setNavGraphPath('connectivity')
 sim.setScanId('2t7WUuJeko7')
 sim.setScreenResolution(640, 480)
 sim.init()
+sim.newEpisode()
 
 heading = 0
 elevation = 0
+location = 0
 while True:
-    sim.makeAction(0, heading, elevation)
+    sim.makeAction(location, heading, elevation)
+    location = 0
+    locations = sim.getState().navigableLocations
     cv2.imshow('displaywin', sim.getState().rgb)
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
+    elif ord('1') <= k <= ord('9'):
+        location = k - ord('0')
+        if location >= len(locations):
+            location = 0
     elif k == 81 or k == ord('a'):
         heading -= math.pi / 180
     elif k == 82 or k == ord('w'):
