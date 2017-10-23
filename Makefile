c6b4a5fb784c4785bd7d08fdc4ee717f6e53cccc
Peter Anderson <peteanderson80@gmail.com>
2017-10-23 14:43:57 +1100

    Added documentation using doxygen

--- Makefile
+++ Makefile
@@ -39,3 +39,6 @@ pybind: lib/MatterSim.o
 
 clean:
 	@rm -f lib/* bin/*
+
+docs:
+	doxygen
--- include/MatterSim.hpp
+++ include/MatterSim.hpp
@@ -20,6 +20,9 @@ namespace mattersim {
 
     typedef std::shared_ptr<Viewpoint> ViewpointPtr;
 
+    /**
+     * Simulator state class.
+     */
     struct SimState {
         unsigned int step;
         cv::Mat rgb;
@@ -32,6 +35,9 @@ namespace mattersim {
 
     typedef std::shared_ptr<SimState> SimStatePtr;
 
+    /**
+     * Class for representing nearby candidate locations that can be moved to.
+     */
     struct Location {
         bool included;
         std::string image_id;
@@ -43,18 +49,68 @@ namespace mattersim {
 
     typedef std::shared_ptr<Location> LocationPtr;
 
+    /**
+     * Main class for accessing an instance of the simulator environment.
+     */
     class Simulator {
     public:
         Simulator() : state{new SimState()} {};
+        
+        /**
+         * Set path to the <a href="https://niessner.github.io/Matterport/">Matterport3D dataset</a>. The provided directory must contain 
+         * subdirectories of the form: "/v1/scans/<scanId>/matterport_skybox_images/".
+         */
         void setDatasetPath(std::string path);
+        
+        /**
+         * Sets path to viewpoint connectivity graphs. Default is "./connectivity".
+         */
         void setNavGraphPath(std::string path);
+        
+        /**
+         * Sets resolution. Default is 320 x 240.
+         */
         void setScreenResolution(int width, int height);
+        
+        /**
+         * Sets which scene is used, e.g. "2t7WUuJeko7".
+         */
         void setScanId(std::string id);
+        
+        /**
+         * Initialize the simulator. Further configuration won't take any effect from now on.
+         */
         void init();
+        
+        /**
+         * Starts a new episode. It is not needed right after init().
+         */
         void newEpisode();
+        
+        /**
+         * Returns true after the end episode action has been selected by the agent 
+         * (after which no more actions can be made).
+         */
         bool isEpisodeFinished();
+        
+        /**
+         * Returns the current environment state including RGB image and available actions.
+         */
         SimStatePtr getState();
+        
+        /** @brief Select an action.
+         *
+         * An RL agent will sample an action here. A task-specific reward can be determined 
+         * based on the location, heading, elevation, etc. of the resulting state.
+         * @param index - an index into the set of feasible actions defined by getState()->navigableLocations.
+         * @param heading - desired heading change in radians
+         * @param elevation - desired elevation change in radians
+         */
         void makeAction(int index, float heading, float elevation);
+        
+        /**
+         * Closes the environment and releases underlying texture resources, OpenGL contexts, etc.
+         */
         void close();
     private:
         void populateNavigable();
