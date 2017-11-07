fcf91969539aba491025ee1b427546ed11ec8a88
Phil Roberts <philip.roberts@adelaide.edu.au>
2017-11-07 17:13:20 +1030

    Match python API to c++ API

--- src/lib_python/MatterSimPython.cpp
+++ src/lib_python/MatterSimPython.cpp
@@ -89,8 +89,8 @@ namespace mattersim {
         void setSeed(int seed) {
             sim.setSeed(seed);
         }
-        void setElevationLimits(double min, double max) {
-            sim.setElevationLimits(min, max);
+        bool setElevationLimits(double min, double max) {
+            return sim.setElevationLimits(min, max);
         }
         void newEpisode(const std::string& scanId, const std::string& viewpointId=std::string(), 
               double heading=0, double elevation=0) {
