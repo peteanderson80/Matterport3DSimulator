7364fe420023b425bf5d609d6ed18cb531170b8c
Peter Anderson <peteanderson80@gmail.com>
2017-11-08 19:56:36 +1030

    Python demo using new robot relative coords

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -3,19 +3,21 @@ sys.path.append('build')
 import MatterSim
 import time
 import math
-
 import cv2
+
+
+WIDTH = 640
+HEIGHT = 480
+VFOV = math.radians(60)
+HFOV = VFOV*WIDTH/HEIGHT
+TEXT_COLOR = [230, 40, 40]
+
 cv2.namedWindow('displaywin')
 sim = MatterSim.Simulator()
-sim.setDatasetPath('../data')
-sim.setNavGraphPath('connectivity')
-sim.setCameraResolution(640, 480)
-#sim.setDiscretizedViewingAngles(True)
+sim.setCameraResolution(WIDTH, HEIGHT)
+sim.setCameraVFOV(VFOV);
 sim.init()
-#sim.newEpisode('2t7WUuJeko7', 'ee3c278732514701bacdfdacf81f4915', 0, 0)
-#sim.newEpisode('2t7WUuJeko7', '8d51f41d9ce04dfdaae944da9c6d3847', 0, 0)
 sim.newEpisode('2t7WUuJeko7', '1e6b606b44df4a6086c0f97e826d4d15', 0, 0)
-
 
 
 heading = 0
@@ -32,16 +34,12 @@ while True:
     im = state.rgb
     origin = locations[0].point
     for idx, loc in enumerate(locations[1:]):
-        angle = math.pi/2 - math.atan2(loc.point[1] - origin[1], loc.point[0] - origin[0])
-        anglediff = angle - state.heading
-        while anglediff > math.pi:
-            anglediff -= 2 * math.pi
-        while anglediff < -math.pi:
-            anglediff += 2 * math.pi
-        if abs(anglediff) < math.pi / 4:
-            dist = math.sqrt((loc.point[0] - origin[0]) ** 2 + (loc.point[1] - origin[1]) ** 2)
-            colour = [230, 40, 40]
-            cv2.putText(im, str(idx + 1), (320 + int(320 * anglediff * 4 / math.pi), 480 - int(dist * 100)), cv2.FONT_HERSHEY_SIMPLEX, 2, colour, thickness=3)
+        # Draw actions on the screen
+        fontScale = 3.0/loc.rel_distance
+        x = WIDTH/2+int(loc.rel_heading/HFOV*WIDTH)
+        y = HEIGHT/2-int(loc.rel_elevation/VFOV*HEIGHT)
+        cv2.putText(im, str(idx + 1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 
+            fontScale, TEXT_COLOR, thickness=3)
     cv2.imshow('displaywin', im)
     k = cv2.waitKey(1)
     if k == ord('q'):
