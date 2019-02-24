31e7e3ed5927d38e03a71444073bcbbc98c462d8
Peter Anderson <peteanderson80@gmail.com>
2019-02-24 11:24:34 -0500

    Fix broken download links for R2R dataset

--- tasks/R2R/data/download.sh
+++ tasks/R2R/data/download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://storage.googleapis.com/bringmeaspoon/R2Rdata/R2R_train.json -P tasks/R2R/data/
-wget https://storage.googleapis.com/bringmeaspoon/R2Rdata/R2R_val_seen.json -P tasks/R2R/data/
-wget https://storage.googleapis.com/bringmeaspoon/R2Rdata/R2R_val_unseen.json -P tasks/R2R/data/
-wget https://storage.googleapis.com/bringmeaspoon/R2Rdata/R2R_test.json -P tasks/R2R/data/
+wget https://www.dropbox.com/s/lztjsji51pr5ig2/R2R_train.json -P tasks/R2R/data/
+wget https://www.dropbox.com/s/66nowglznzx1le9/R2R_val_seen.json -P tasks/R2R/data/
+wget https://www.dropbox.com/s/it9zpexb97d6bes/R2R_val_unseen.json -P tasks/R2R/data/
+wget https://www.dropbox.com/s/0huat2lc5iy5o8j/R2R_test.json -P tasks/R2R/data/
